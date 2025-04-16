e25d9edf37b57031bc36612e7fa92f07b81f06bc
Tuyen Le <lethikimtuyen87@gmail.com>
2025-04-16 15:52:04 +0200

    feat & refactor: add function to convert netcdf or grib file to pd.dataframe, refactor dependencies

--- onehealth_db/database.py
+++ onehealth_db/database.py
@@ -1,9 +1,56 @@
 from pathlib import Path
 import duckdb
 import pandas as pd
+import xarray as xr
+from typing import Union, List, Literal
 
 
 DEFAULT_DB_PATH = Path("data/onehealth.db")
+
+
+def file_to_dataframe(
+    file_path: Path, columns: Union[List[str], Literal["all", "default"]] = "default"
+) -> pd.DataFrame:
+    """Convert a netCDF/GRIB file to a pandas DataFrame.
+
+    Args:
+        file_path (Path): The path to the file.
+        columns (Union[List[str], Literal["all", "default"]]):
+            The columns to include in the DataFrame.
+            If "all", all columns are included.
+            If "default", default columns are included.
+            If a list, only the specified columns are included.
+
+    Returns:
+        pd.DataFrame: The DataFrame containing the data from the file.
+    """
+    if not file_path.exists():
+        raise FileNotFoundError(f"File {file_path} does not exist.")
+
+    if not columns:
+        raise ValueError("Columns must be provided.")
+
+    if columns not in ["all", "default"] and not isinstance(columns, list):
+        raise ValueError("Columns must be 'all', 'default', or a list of column names.")
+
+    if columns == "default":
+        columns = ["valid_time", "latitude", "longitude", "t2m"]
+
+    suffix = file_path.suffix.lower()
+
+    if suffix not in [".nc", ".grib", ".grb"]:
+        raise ValueError(f"Unsupported file format: {suffix}")
+
+    engine = "netcdf4" if suffix == ".nc" else "cfgrib"
+    with xr.open_dataset(file_path, engine=engine, decode_timedelta=False) as ds:
+        df = ds.to_dataframe().reset_index()
+        if columns == "all":
+            return df
+        if not all(col in df.columns for col in columns):
+            raise ValueError(f"Some columns are not present in the file: {columns}")
+        # Select only the specified columns
+        df = df[columns]
+        return df
 
 
 def import_data(
--- onehealth_db/test/test_database.py
+++ onehealth_db/test/test_database.py
@@ -1,6 +1,7 @@
 import pytest
 from onehealth_db import database
 import pandas as pd
+from pathlib import Path
 
 
 def check_table_name(con, table_name):
@@ -70,3 +71,77 @@ def test_import_data_custom(get_dataframe, tmp_path):
     # Clean up
     custom_db_path.unlink()
     custom_db_path.parent.rmdir()
+
+
+def test_file_to_dataframe_invalid(tmp_path):
+    # invalid file
+    with pytest.raises(FileNotFoundError):
+        database.file_to_dataframe(tmp_path / "non_existent_file.nc")
+
+    # unsupported file format
+    invalid_file = tmp_path / "invalid_file.txt"
+    invalid_file.write_text("This is not a valid file format.")
+
+    with pytest.raises(ValueError):
+        database.file_to_dataframe(invalid_file)
+
+    # empty columns
+    with pytest.raises(ValueError):
+        database.file_to_dataframe(invalid_file, columns=[])
+
+    # invalid col type
+    with pytest.raises(ValueError):
+        database.file_to_dataframe(invalid_file, columns=123)
+
+    # unsupported col
+    with pytest.raises(ValueError):
+        database.file_to_dataframe(invalid_file, columns=["unsupported_column"])
+
+
+@pytest.mark.filterwarnings("ignore::RuntimeWarning")
+def test_file_to_dataframe_netcdf(get_dataframe, tmp_path):
+    ds = get_dataframe.to_xarray()
+
+    netcdf_file = tmp_path / "test_file.nc"
+    ds.to_netcdf(netcdf_file, mode="a")
+    # Convert to DataFrame
+    df_netcdf = database.file_to_dataframe(netcdf_file, columns=["column1", "column2"])
+    assert isinstance(df_netcdf, pd.DataFrame)
+    assert df_netcdf.shape == (3, 2)
+    assert "column1" in df_netcdf.columns
+    assert "column2" in df_netcdf.columns
+    # Clean up
+    netcdf_file.unlink()
+    tmp_path.rmdir()
+
+
+def test_file_to_dataframe_grib_col_default():
+    grib_file = Path("onehealth_db/test/data/era5_2025_03_monthly_area_1-1-0-m1.grib")
+    # Convert to DataFrame
+    df_grib = database.file_to_dataframe(grib_file, columns="default")
+    assert isinstance(df_grib, pd.DataFrame)
+    assert df_grib.shape[1] == 4
+    assert "valid_time" in df_grib.columns
+    assert "latitude" in df_grib.columns
+    assert "longitude" in df_grib.columns
+    assert "t2m" in df_grib.columns
+    # Clean up idx file due to cfgrib
+    idx_files = grib_file.parent.glob("*.idx")
+    for idx_file in idx_files:
+        idx_file.unlink()
+
+
+def test_file_to_dataframe_grib_col_all():
+    grib_file = Path("onehealth_db/test/data/era5_2025_03_monthly_area_1-1-0-m1.grib")
+    # Convert to DataFrame
+    df_grib = database.file_to_dataframe(grib_file, columns="all")
+    assert isinstance(df_grib, pd.DataFrame)
+    # download grib file has 8 columns
+    # latitude, longitude, number, time, step, surface, valid_time, t2m
+    assert df_grib.shape[1] == 8
+    assert "valid_time" in df_grib.columns
+    assert "time" in df_grib.columns
+    # Clean up idx file due to cfgrib
+    idx_files = grib_file.parent.glob("*.idx")
+    for idx_file in idx_files:
+        idx_file.unlink()
