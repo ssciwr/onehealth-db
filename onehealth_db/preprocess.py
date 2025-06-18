83685ec043f0b8ce1a3910be23998a9fa8e79123
Tuyen Le <lethikimtuyen87@gmail.com>
2025-06-18 15:08:08 +0200

    feat & refactor: add function converting m to mm and refactor other functions

--- onehealth_db/preprocess.py
+++ onehealth_db/preprocess.py
@@ -4,7 +4,7 @@ import numpy as np
 import warnings
 
 
-T = TypeVar("T", bound=Union[float, xr.DataArray])
+T = TypeVar("T", bound=Union[np.float64, xr.DataArray])
 
 
 def convert_360_to_180(longitude: T) -> T:
@@ -20,79 +20,32 @@ def convert_360_to_180(longitude: T) -> T:
 
 
 def adjust_longitude_360_to_180(
-    dataset: xr.Dataset, inplace: bool = True
+    dataset: xr.Dataset,
+    limited_area: bool = False,
+    var_name: str = "longitude",
 ) -> xr.Dataset:
     """Adjust longitude from 0-360 to -180-180.
 
     Args:
         dataset (xr.Dataset): Dataset with longitude in 0-360 range.
-        inplace (bool): If True, modify the original dataset.
-            If False, return a new dataset. Default is True.
+        limited_area (bool): Flag indicating if the dataset is a limited area.
+            Default is False.
+        var_name (str): Name of the longitude variable in the dataset.
+            Default is "longitude".
 
     Returns:
         xr.Dataset: Dataset with longitude adjusted to -180-180 range.
     """
-    if not inplace:
-        dataset = dataset.copy(deep=True)
-
     # record attributes
-    lon_attrs = dataset["longitude"].attrs.copy()
+    lon_attrs = dataset[var_name].attrs.copy()
 
     # adjust longitude
     dataset = dataset.assign_coords(
-        longitude=convert_360_to_180(dataset["longitude"])
-    ).sortby("longitude")
-    dataset["longitude"].attrs = lon_attrs
+        longitude=convert_360_to_180(dataset[var_name])
+    ).sortby(var_name)
+    dataset[var_name].attrs = lon_attrs
 
-    return dataset
-
-
-def convert_to_celsius(temperature_kelvin: xr.DataArray) -> xr.DataArray:
-    """Convert temperature from Kelvin to Celsius.
-
-    Args:
-        temperature_kelvin (xr.DataArray): Temperature in Kelvin,
-            accessed through t2m variable in the dataset.
-
-    Returns:
-        xr.DataArray: Temperature in Celsius.
-    """
-    return temperature_kelvin - 273.15
-
-
-def convert_to_celsius_with_attributes(
-    dataset: xr.Dataset, limited_area: bool = True, inplace: bool = True
-) -> xr.Dataset:
-    """Convert temperature from Kelvin to Celsius and keep attributes.
-
-    Args:
-        dataset (xr.Dataset): Dataset containing temperature in Kelvin.
-        limited_area (bool): Flag indicating if the dataset is a limited area.
-            Default is True.
-        inplace (bool): If True, modify the original dataset.
-            If False, return a new dataset. Default is True.
-
-    Returns:
-        xr.Dataset: Dataset with temperature converted to Celsius.
-    """
-    if not inplace:
-        dataset = dataset.copy(deep=True)
-
-    # record attributes
-    t2m_attrs = dataset["t2m"].attrs.copy()
-
-    # Convert temperature variable
-    dataset["t2m"] = convert_to_celsius(dataset["t2m"])
-
-    # Update attributes
-    dataset["t2m"].attrs = t2m_attrs
-    dataset["t2m"].attrs.update(
-        {
-            "GRIB_units": "C",
-            "units": "C",
-        }
-    )
-
+    # update attributes of data variables
     for var in dataset.data_vars.keys():
         if limited_area:
             # get old attribute values
@@ -119,6 +72,57 @@ def convert_to_celsius_with_attributes(
                     "GRIB_longitudeOfLastGridPointInDegrees": np.float64(180.0),
                 }
             )
+
+    return dataset
+
+
+def convert_to_celsius(temperature_kelvin: T) -> T:
+    """Convert temperature from Kelvin to Celsius.
+
+    Args:
+        temperature_kelvin (T): Temperature in Kelvin,
+            accessed through t2m variable in the dataset.
+
+    Returns:
+        T: Temperature in Celsius.
+    """
+    return temperature_kelvin - 273.15
+
+
+def convert_to_celsius_with_attributes(
+    dataset: xr.Dataset,
+    inplace: bool = False,
+    var_name: str = "t2m",
+) -> xr.Dataset:
+    """Convert temperature from Kelvin to Celsius and keep attributes.
+
+    Args:
+        dataset (xr.Dataset): Dataset containing temperature in Kelvin.
+        inplace (bool): If True, modify the original dataset.
+            If False, return a new dataset. Default is False.
+        var_name (str): Name of the temperature variable in the dataset.
+            Default is "t2m".
+
+    Returns:
+        xr.Dataset: Dataset with temperature converted to Celsius.
+    """
+    if not inplace:
+        dataset = dataset.copy(deep=True)
+
+    # record attributes
+    var_attrs = dataset[var_name].attrs.copy()
+
+    # Convert temperature variable
+    dataset[var_name] = convert_to_celsius(dataset[var_name])
+
+    # Update attributes
+    dataset[var_name].attrs = var_attrs
+    dataset[var_name].attrs.update(
+        {
+            "GRIB_units": "C",
+            "units": "C",
+        }
+    )
 
     return dataset
 
@@ -154,5 +158,53 @@ def rename_coords(dataset: xr.Dataset, coords_mapping: dict) -> xr.Dataset:
                 f"Coordinate '{old_name}' not found in the dataset and will be skipped.",
                 UserWarning,
             )
+
+    return dataset
+
+
+def convert_m_to_mm(precipitation: T) -> T:
+    """Convert precipitation from meters to millimeters.
+
+    Args:
+        precipitation (T): Precipitation in meters.
+
+    Returns:
+        T: Precipitation in millimeters.
+    """
+    return precipitation * 1000.0
+
+
+def convert_m_to_mm_with_attributes(
+    dataset: xr.Dataset, inplace: bool = False, var_name: str = "tp"
+) -> xr.Dataset:
+    """Convert precipitation from meters to millimeters and keep attributes.
+
+    Args:
+        dataset (xr.Dataset): Dataset containing precipitation in meters.
+        inplace (bool): If True, modify the original dataset.
+            If False, return a new dataset. Default is False.
+        var_name (str): Name of the precipitation variable in the dataset.
+            Default is "tp".
+
+    Returns:
+        xr.Dataset: Dataset with precipitation converted to millimeters.
+    """
+    if not inplace:
+        dataset = dataset.copy(deep=True)
+
+    # record attributes
+    var_attrs = dataset[var_name].attrs.copy()
+
+    # Convert precipitation variable
+    dataset[var_name] = convert_m_to_mm(dataset[var_name])
+
+    # Update attributes
+    dataset[var_name].attrs = var_attrs
+    dataset[var_name].attrs.update(
+        {
+            "GRIB_units": "mm",
+            "units": "mm",
+        }
+    )
 
     return dataset
--- onehealth_db/test/test_preprocess.py
+++ onehealth_db/test/test_preprocess.py
@@ -6,27 +6,55 @@ from onehealth_db import preprocess
 
 @pytest.fixture()
 def get_data():
+    time_points = np.array(["2024-01-01", "2025-01-01"], dtype="datetime64")
+    latitude = [0, 1]
+    longitude = [0, 1, 2]
+    longitude_first = np.float64(0.0)
+    longitude_last = np.float64(359.9)
+
+    # create random data for t2m and tp
     rng = np.random.default_rng(seed=42)
-    data = rng.random((2, 3)) * 1000 + 273.15
-    data_array = xr.DataArray(
+    data = rng.random((2, 2, 3)) * 1000 + 273.15
+    data_array_t2m = xr.DataArray(
         data,
-        dims=["latitude", "longitude"],
-        coords={"latitude": [0, 1], "longitude": [0, 1, 2]},
+        dims=["time", "latitude", "longitude"],
+        coords={"time": time_points, "latitude": latitude, "longitude": longitude},
     )
-    return data_array
+
+    data = rng.random((2, 2, 3)) / 1000
+    data_array_precip = xr.DataArray(
+        data,
+        dims=["time", "latitude", "longitude"],
+        coords={"time": time_points, "latitude": latitude, "longitude": longitude},
+    )
+    data_array_t2m.attrs = {
+        "GRIB_units": "K",
+        "units": "K",
+        "GRIB_longitudeOfFirstGridPointInDegrees": longitude_first,
+        "GRIB_longitudeOfLastGridPointInDegrees": longitude_last,
+    }
+    data_array_precip.attrs = {
+        "GRIB_units": "m",
+        "units": "m",
+        "GRIB_longitudeOfFirstGridPointInDegrees": longitude_first,
+        "GRIB_longitudeOfLastGridPointInDegrees": longitude_last,
+    }
+    return data_array_t2m, data_array_precip
 
 
 @pytest.fixture()
 def get_dataset(get_data):
-    dataset = xr.Dataset({"t2m": get_data})
+    data_t2m = get_data[0]
+    data_tp = get_data[1]
+    dataset = xr.Dataset(
+        {"t2m": data_t2m, "tp": data_tp},
+        coords={
+            "time": data_t2m.time,
+            "latitude": data_t2m.latitude,
+            "longitude": data_t2m.longitude,
+        },
+    )
     # create attributes for the dataset
-    attrs = {
-        "GRIB_units": "K",
-        "units": "K",
-        "GRIB_longitudeOfFirstGridPointInDegrees": np.float64(0.0),
-        "GRIB_longitudeOfLastGridPointInDegrees": np.float64(359.9),
-    }
-    dataset["t2m"].attrs.update(attrs)
     dataset.attrs.update({"GRIB_centre": "ecmf"})
     dataset["longitude"].attrs.update({"units": "degrees_east"})
     return dataset
@@ -34,8 +62,9 @@ def get_dataset(get_data):
 
 def test_convert_360_to_180(get_data):
     # convert 360 to 180, xarray
-    converted_array = preprocess.convert_360_to_180(get_data)
-    expected_array = (get_data + 180) % 360 - 180
+    t2m_data = get_data[0]
+    converted_array = preprocess.convert_360_to_180(t2m_data)
+    expected_array = (t2m_data + 180) % 360 - 180
     assert np.allclose(converted_array.values, expected_array.values)
     assert converted_array.dims == expected_array.dims
     assert all(
@@ -68,9 +97,10 @@ def test_convert_360_to_180(get_data):
     assert np.isclose(converted_num, num)
 
 
-def test_adjust_longitude_360_to_180_no_inplace(get_dataset):
+def test_adjust_longitude_360_to_180(get_dataset):
+    # full area
     adjusted_dataset = preprocess.adjust_longitude_360_to_180(
-        get_dataset, inplace=False
+        get_dataset, limited_area=False
     )
     expected_dataset = get_dataset.assign_coords(
         longitude=((get_dataset.longitude + 180) % 360 - 180)
@@ -78,8 +108,17 @@ def test_adjust_longitude_360_to_180_no_inplace(get_dataset):
 
     # check if the attributes are preserved
     assert adjusted_dataset.attrs == get_dataset.attrs
-    assert adjusted_dataset["t2m"].attrs == get_dataset["t2m"].attrs
+    assert adjusted_dataset["t2m"].attrs.get("units") == get_dataset["t2m"].attrs.get(
+        "units"
+    )
     assert adjusted_dataset["longitude"].attrs == get_dataset["longitude"].attrs
+    for var in adjusted_dataset.data_vars.keys():
+        assert adjusted_dataset[var].attrs.get(
+            "GRIB_longitudeOfFirstGridPointInDegrees"
+        ) == np.float64(-179.9)
+        assert adjusted_dataset[var].attrs.get(
+            "GRIB_longitudeOfLastGridPointInDegrees"
+        ) == np.float64(180.0)
 
     # check if the data is adjusted correctly
     assert np.allclose(adjusted_dataset["t2m"].values, expected_dataset["t2m"].values)
@@ -89,40 +128,31 @@ def test_adjust_longitude_360_to_180_no_inplace(get_dataset):
         for dim in adjusted_dataset["t2m"].dims
     )
 
-
-def test_adjust_longitude_360_to_180_inplace(get_dataset):
-    org_dataset = get_dataset.copy(deep=True)
-    adjusted_dataset = preprocess.adjust_longitude_360_to_180(get_dataset, inplace=True)
-    expected_dataset = org_dataset.assign_coords(
-        longitude=((get_dataset.longitude + 180) % 360 - 180)
-    ).sortby("longitude")
-
-    # check if the attributes are preserved
-    assert adjusted_dataset.attrs == get_dataset.attrs
-    assert adjusted_dataset["t2m"].attrs == get_dataset["t2m"].attrs
-    assert adjusted_dataset["longitude"].attrs == get_dataset["longitude"].attrs
-
-    # check if the data is adjusted correctly
-    assert np.allclose(adjusted_dataset["t2m"].values, expected_dataset["t2m"].values)
-    assert adjusted_dataset["t2m"].dims == expected_dataset["t2m"].dims
-    assert all(
-        adjusted_dataset["t2m"].coords[dim].equals(expected_dataset["t2m"].coords[dim])
-        for dim in adjusted_dataset["t2m"].dims
+    # limited area
+    for var in get_dataset.data_vars.keys():
+        get_dataset[var].attrs.update(
+            {
+                "GRIB_longitudeOfFirstGridPointInDegrees": np.float64(-45.0),
+                "GRIB_longitudeOfLastGridPointInDegrees": np.float64(45.0),
+            }
+        )
+    adjusted_dataset = preprocess.adjust_longitude_360_to_180(
+        get_dataset, limited_area=True
     )
-
-    # check if the original dataset is modified
-    assert np.allclose(get_dataset["t2m"].values, expected_dataset["t2m"].values)
-    assert get_dataset["t2m"].dims == expected_dataset["t2m"].dims
-    assert all(
-        get_dataset["t2m"].coords[dim].equals(expected_dataset["t2m"].coords[dim])
-        for dim in get_dataset["t2m"].dims
-    )
+    for var in adjusted_dataset.data_vars.keys():
+        assert adjusted_dataset[var].attrs.get(
+            "GRIB_longitudeOfFirstGridPointInDegrees"
+        ) == np.float64(-45.0)
+        assert adjusted_dataset[var].attrs.get(
+            "GRIB_longitudeOfLastGridPointInDegrees"
+        ) == np.float64(45.0)
 
 
 def test_convert_to_celsius(get_data):
-    # convert to Celsius
-    celsius_array = preprocess.convert_to_celsius(get_data)
-    expected_celsius_array = get_data - 273.15
+    t2m_data = get_data[0]
+    # convert to Celsius, xarray
+    celsius_array = preprocess.convert_to_celsius(t2m_data)
+    expected_celsius_array = t2m_data - 273.15
     assert np.allclose(celsius_array.values, expected_celsius_array.values)
     assert celsius_array.dims == expected_celsius_array.dims
     assert all(
@@ -130,11 +160,17 @@ def test_convert_to_celsius(get_data):
         for dim in celsius_array.dims
     )
 
+    # float numbers
+    kelvin_temp = 300.0
+    celsius_temp = preprocess.convert_to_celsius(kelvin_temp)
+    expected_temp = kelvin_temp - 273.15
+    assert np.isclose(celsius_temp, expected_temp)
+
 
 def test_convert_to_celsius_with_attributes_no_inplace(get_dataset):
-    # convert to Celsius, no area
+    # convert to Celsius
     celsius_dataset = preprocess.convert_to_celsius_with_attributes(
-        get_dataset, limited_area=False, inplace=False
+        get_dataset, inplace=False
     )
     expected_celsius_array = get_dataset["t2m"] - 273.15
 
@@ -142,12 +178,6 @@ def test_convert_to_celsius_with_attributes_no_inplace(get_dataset):
     assert celsius_dataset.attrs == get_dataset.attrs
     assert celsius_dataset["t2m"].attrs.get("GRIB_units") == "C"
     assert celsius_dataset["t2m"].attrs.get("units") == "C"
-    assert celsius_dataset["t2m"].attrs.get(
-        "GRIB_longitudeOfFirstGridPointInDegrees"
-    ) == np.float64(-179.9)
-    assert celsius_dataset["t2m"].attrs.get(
-        "GRIB_longitudeOfLastGridPointInDegrees"
-    ) == np.float64(180.0)
 
     # check if the data is converted correctly
     assert np.allclose(celsius_dataset["t2m"].values, expected_celsius_array.values)
@@ -157,46 +187,18 @@ def test_convert_to_celsius_with_attributes_no_inplace(get_dataset):
         for dim in celsius_dataset["t2m"].dims
     )
 
-    # convert to Celsius, limited area
-    get_dataset["t2m"].attrs.update(
-        {
-            "GRIB_longitudeOfFirstGridPointInDegrees": np.float64(-45.0),
-            "GRIB_longitudeOfLastGridPointInDegrees": np.float64(45.0),
-        }
-    )
-    celsius_dataset = preprocess.convert_to_celsius_with_attributes(
-        get_dataset, limited_area=True, inplace=False
-    )
-    assert celsius_dataset.attrs == get_dataset.attrs
-    assert celsius_dataset["t2m"].attrs.get("GRIB_units") == "C"
-    assert celsius_dataset["t2m"].attrs.get("units") == "C"
-    assert celsius_dataset["t2m"].attrs.get(
-        "GRIB_longitudeOfFirstGridPointInDegrees"
-    ) == np.float64(-45.0)
-    assert celsius_dataset["t2m"].attrs.get(
-        "GRIB_longitudeOfLastGridPointInDegrees"
-    ) == np.float64(45.0)
-
 
 def test_convert_to_celsius_with_attributes_inplace(get_dataset):
-    # convert to Celsius, no area
+    # convert to Celsius
     org_data_array = get_dataset["t2m"].copy()
     org_ds_attrs = get_dataset.attrs.copy()
-    preprocess.convert_to_celsius_with_attributes(
-        get_dataset, limited_area=False, inplace=True
-    )
+    preprocess.convert_to_celsius_with_attributes(get_dataset, inplace=True)
     expected_celsius_array = org_data_array - 273.15
 
     # check if the attributes are preserved
     assert get_dataset.attrs == org_ds_attrs
     assert get_dataset["t2m"].attrs.get("GRIB_units") == "C"
     assert get_dataset["t2m"].attrs.get("units") == "C"
-    assert get_dataset["t2m"].attrs.get(
-        "GRIB_longitudeOfFirstGridPointInDegrees"
-    ) == np.float64(-179.9)
-    assert get_dataset["t2m"].attrs.get(
-        "GRIB_longitudeOfLastGridPointInDegrees"
-    ) == np.float64(180.0)
 
     # check if the data is converted correctly
     assert np.allclose(get_dataset["t2m"].values, expected_celsius_array.values)
@@ -205,34 +207,6 @@ def test_convert_to_celsius_with_attributes_inplace(get_dataset):
         get_dataset["t2m"].coords[dim].equals(expected_celsius_array.coords[dim])
         for dim in get_dataset["t2m"].dims
     )
-
-    # check if the original dataset is modified
-    assert np.allclose(get_dataset["t2m"].values, expected_celsius_array.values)
-    assert get_dataset["t2m"].dims == expected_celsius_array.dims
-    assert all(
-        get_dataset["t2m"].coords[dim].equals(expected_celsius_array.coords[dim])
-        for dim in get_dataset["t2m"].dims
-    )
-
-    # convert to Celsius, limited area
-    get_dataset["t2m"].attrs.update(
-        {
-            "GRIB_longitudeOfFirstGridPointInDegrees": np.float64(-45.0),
-            "GRIB_longitudeOfLastGridPointInDegrees": np.float64(45.0),
-        }
-    )
-    preprocess.convert_to_celsius_with_attributes(
-        get_dataset, limited_area=True, inplace=True
-    )
-    assert get_dataset.attrs == org_ds_attrs
-    assert get_dataset["t2m"].attrs.get("GRIB_units") == "C"
-    assert get_dataset["t2m"].attrs.get("units") == "C"
-    assert get_dataset["t2m"].attrs.get(
-        "GRIB_longitudeOfFirstGridPointInDegrees"
-    ) == np.float64(-45.0)
-    assert get_dataset["t2m"].attrs.get(
-        "GRIB_longitudeOfLastGridPointInDegrees"
-    ) == np.float64(45.0)
 
 
 def test_rename_coords(get_dataset):
@@ -273,3 +247,64 @@ def test_rename_coords_notexist_coords(get_dataset):
     assert "longitude" in renamed_dataset.coords
     assert "latitude" not in renamed_dataset.coords
     assert "lat" in renamed_dataset.coords
+
+
+def test_convert_m_to_mm(get_data):
+    tp_data = get_data[1]
+    # convert m to mm, xarray
+    mm_array = preprocess.convert_m_to_mm(tp_data)
+    expected_mm_array = tp_data * 1000.0
+    assert np.allclose(mm_array.values, expected_mm_array.values)
+    assert mm_array.dims == expected_mm_array.dims
+    assert all(
+        mm_array.coords[dim].equals(expected_mm_array.coords[dim])
+        for dim in mm_array.dims
+    )
+
+    # float numbers
+    m_precip = 0.001
+    mm_precip = preprocess.convert_m_to_mm(m_precip)
+    expected_precip = m_precip * 1000.0
+    assert np.isclose(mm_precip, expected_precip)
+
+
+def test_convert_m_to_mm_with_attributes_no_inplace(get_dataset):
+    # convert m to mm
+    mm_dataset = preprocess.convert_m_to_mm_with_attributes(
+        get_dataset, inplace=False, var_name="tp"
+    )
+    expected_mm_array = get_dataset["tp"] * 1000.0
+
+    # check if the attributes are preserved
+    assert mm_dataset.attrs == get_dataset.attrs
+    assert mm_dataset["tp"].attrs.get("GRIB_units") == "mm"
+    assert mm_dataset["tp"].attrs.get("units") == "mm"
+
+    # check if the data is converted correctly
+    assert np.allclose(mm_dataset["tp"].values, expected_mm_array.values)
+    assert mm_dataset["tp"].dims == expected_mm_array.dims
+    assert all(
+        mm_dataset["tp"].coords[dim].equals(expected_mm_array.coords[dim])
+        for dim in mm_dataset["tp"].dims
+    )
+
+
+def test_convert_m_to_mm_with_attributes_inplace(get_dataset):
+    # convert m to mm
+    org_data_array = get_dataset["tp"].copy()
+    org_ds_attrs = get_dataset.attrs.copy()
+    preprocess.convert_m_to_mm_with_attributes(get_dataset, inplace=True, var_name="tp")
+    expected_mm_array = org_data_array * 1000.0
+
+    # check if the attributes are preserved
+    assert get_dataset.attrs == org_ds_attrs
+    assert get_dataset["tp"].attrs.get("GRIB_units") == "mm"
+    assert get_dataset["tp"].attrs.get("units") == "mm"
+
+    # check if the data is converted correctly
+    assert np.allclose(get_dataset["tp"].values, expected_mm_array.values)
+    assert get_dataset["tp"].dims == expected_mm_array.dims
+    assert all(
+        get_dataset["tp"].coords[dim].equals(expected_mm_array.coords[dim])
+        for dim in get_dataset["tp"].dims
+    )
