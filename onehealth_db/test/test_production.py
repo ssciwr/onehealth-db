31b99725fe0b545aded2994b754c9bfa74b58b9c
Inga Ulusoy <inga.ulusoy@uni-heidelberg.de>
2025-07-09 12:11:20 +0200

    fix: resolve typos

--- onehealth_db/test/test_production.py
+++ onehealth_db/test/test_production.py
@@ -57,7 +57,7 @@ def test_get_production_data(tmp_path: Path):
 def test_create_directories(tmp_path: Path):
     prod.create_directories(str(tmp_path) + "test")
     testdir = tmp_path / "test"
-    assert testdir.exists
+    assert testdir.exists()
 
 
 def test_get_engine():
